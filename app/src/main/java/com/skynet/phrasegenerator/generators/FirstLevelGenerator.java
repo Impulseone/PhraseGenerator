333f90c7cce9cd7990f31a6a428d72d7de9f65b2
Sergei <sk-new@mail.ru>
2020-07-29 11:40:56 +0300

    new words and phrases added. First level generator complete.

--- app/src/main/java/com/skynet/phrasegenerator/generators/FirstLevelGenerator.java
+++ app/src/main/java/com/skynet/phrasegenerator/generators/FirstLevelGenerator.java
@@ -1,5 +1,7 @@
 package com.skynet.phrasegenerator.generators;
 
+import com.skynet.phrasegenerator.entity.Adjective;
+import com.skynet.phrasegenerator.entity.Noun;
 import com.skynet.phrasegenerator.helpers.WordsStore;
 
 import java.util.Random;
@@ -15,7 +17,7 @@ public class FirstLevelGenerator extends Generator {
 
     @Override
     public String generatePhrase() {
-        int k = 1 + random.nextInt(6);
+        int k = 1 + random.nextInt(7);
         if (k == randomPhraseNumber) return generatePhrase();
         else randomPhraseNumber = k;
         switch (k) {
@@ -31,61 +33,65 @@ public class FirstLevelGenerator extends Generator {
                 return fifthFigure();
             case 6:
                 return sixthFigure();
+            case 7:
+                return sevenFigure();
         }
         return null;
     }
 
     private String firstFigure() {
-        // Все коты - это укропы.
+        // Коты - это укропы.
         // Башмаки - это коты.
         String nounA = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounB = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounC = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         if (checkEquals(new String[]{nounA, nounB, nounC})) return firstFigure();
-        return "Все " + nounA + " - это " + nounB + "." + "\n" + toFirstUpperCase(nounC) + " - это " + nounA + ".";
+        return toFirstUpperCase(nounA) + getRandomUnion() + nounB + "." + "\n" + toFirstUpperCase(nounC) + getRandomUnion() + nounA + ".";
     }
 
     private String secondFigure() {
-        // Все коты обладают свойством мягкий.
+        // Коты обладают свойством мягкий.
         // Башмаки не обладают свойством мягкий.
         String nounA = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounB = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String adjA = wordsStore.getAdjectives().get(random.nextInt(wordsStore.getAdjectives().size() - 1)).getSingularMale();
         if (checkEquals(new String[]{nounA, nounB})) return secondFigure();
-        return "Все " + nounA + " обладают свойством " + adjA + "." + "\n" + toFirstUpperCase(nounB) + " не обладают свойством " + adjA + ".";
+        return toFirstUpperCase(nounA) + getRandomProperty() + adjA + "." + "\n" + toFirstUpperCase(nounB) + getRandomProperty() + adjA + ".";
     }
 
     private String thirdFigure() {
-        // Все коты - это укропы.
-        // Все коты - это гречки.
+        // Коты - это укропы.
+        // Коты - это гречки.
         String nounA = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounB = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounC = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         if (checkEquals(new String[]{nounA, nounB, nounC})) return thirdFigure();
-        return "Все " + nounA + " - это " + nounB + "." + "\n" + "Все " + nounA + " - это " + nounC + ".";
+        return toFirstUpperCase(nounA) + getRandomUnion() + nounB + "." + "\n" + toFirstUpperCase(nounA) + getRandomUnion() + nounC + ".";
     }
 
     private String fourthFigure() {
-        // Все коты - это укропы.
-        // Все укропы - это не гречки.
+        // Коты - это укропы.
+        // Укропы - это не гречки.
         String nounA = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounB = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounC = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         if (checkEquals(new String[]{nounA, nounB, nounC})) return fourthFigure();
-        return "Все " + nounA + " - это " + nounB + "." + "\n" + "Все " + nounB + " - это не " + nounC + ".";
+
+        return toFirstUpperCase(nounA) + getRandomUnion() + nounB + "." + "\n" + toFirstUpperCase(nounB) + getRandomUnion() + nounC + ".";
     }
 
     private String fifthFigure() {
-        // Все коты - это укропы.
+        // Коты - это укропы.
         // Лагеря и занавесы - это коты.
         String nounA = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounB = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounC = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         String nounD = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
         if (checkEquals(new String[]{nounA, nounB, nounC, nounD})) return fifthFigure();
-        String variantOne = "Все " + nounA + " - это " + nounB + "." + "\n" + toFirstUpperCase(nounC) + " и " + nounD + " - это " + nounA + ".";
-        String variantTwo = toFirstUpperCase(nounC) + " и " + nounD + " - это " + nounA + "." + "\n" + toFirstUpperCase(nounA) + " - это " + nounB + ".";
+        String variantOne = toFirstUpperCase(nounA) + getRandomUnion() + nounB + "." + "\n" + toFirstUpperCase(nounC) + " и " + nounD + getRandomUnion() + nounA + ".";
+        String variantTwo = toFirstUpperCase(nounC) + " и " + nounD + getRandomUnion() + nounA + "." + "\n" + toFirstUpperCase(nounA) + getRandomUnion() + nounB + ".";
         String[] variants = new String[]{variantOne, variantTwo};
+
         return variants[random.nextInt(2)];
     }
 
@@ -94,15 +100,37 @@ public class FirstLevelGenerator extends Generator {
         // Изгибы неправильные.
         // Неправильные предметы служат.
         String nounAPlural = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getPlural();
-        String nounASingular = wordsStore.getNouns().get(random.nextInt(wordsStore.getNouns().size() - 1)).getSingular();
         String adjAPlural = wordsStore.getAdjectives().get(random.nextInt(wordsStore.getAdjectives().size() - 1)).getPlural();
-        String adjASingular = wordsStore.getAdjectives().get(random.nextInt(wordsStore.getAdjectives().size() - 1)).getSingularMale();
         String verbA = wordsStore.getVerbs().get(random.nextInt(wordsStore.getVerbs().size() - 1)).getPlural();
-        String variantOne = toFirstUpperCase(nounAPlural) + " " + adjAPlural + "." + "\n" + toFirstUpperCase(adjAPlural) + " предметы " + verbA + ". ";
-        String variantTwo = toFirstUpperCase(adjAPlural) + " предметы " + verbA + ". " + "\n" + toFirstUpperCase(nounAPlural) + " " + adjAPlural + ".";
-        String variantThree = toFirstUpperCase(nounASingular) + " " + adjASingular + "." + "\n" + toFirstUpperCase(adjAPlural) + " предметы " + verbA + ". ";
-        String variantFour = toFirstUpperCase(adjAPlural) + " предметы " + verbA + ". " + "\n" + toFirstUpperCase(nounASingular) + " " + adjASingular + ".";
-        String[] variants = new String[]{variantOne, variantTwo, variantThree, variantFour};
-        return variants[random.nextInt(4)];
+
+        String variantOne = toFirstUpperCase(nounAPlural) + " " + adjAPlural + "." + "\n" + toFirstUpperCase(adjAPlural) + getRandomSubject() + verbA + ". ";
+        String variantTwo = toFirstUpperCase(adjAPlural) + getRandomSubject() + verbA + ". " + "\n" + toFirstUpperCase(nounAPlural) + " " + adjAPlural + ".";
+        String[] variants = new String[]{variantOne, variantTwo};
+
+        return variants[random.nextInt(2)];
+    }
+
+    private String sevenFigure() {
+        // Неправильные предметы служат.
+        // Изгиб неправильный.
+        String adjASingular;
+        int nounANumber = random.nextInt(wordsStore.getNouns().size() - 1);
+        int adjANumber = random.nextInt(wordsStore.getAdjectives().size() - 1);
+        Noun nounA = wordsStore.getNouns().get(nounANumber);
+        Adjective adjectiveA = wordsStore.getAdjectives().get(adjANumber);
+        if (nounA.getGender().equals("м"))
+            adjASingular = adjectiveA.getSingularMale();
+        else
+            adjASingular = adjectiveA.getSingularFemale();
+
+        String nounASingular = wordsStore.getNouns().get(nounANumber).getSingular();
+        String adjAPlural = wordsStore.getAdjectives().get(adjANumber).getPlural();
+        String verbA = wordsStore.getVerbs().get(random.nextInt(wordsStore.getVerbs().size() - 1)).getPlural();
+
+        String variantThree = toFirstUpperCase(nounASingular) + " " + adjASingular + "." + "\n" + toFirstUpperCase(adjAPlural) + getRandomSubject() + verbA + ". ";
+        String variantFour = toFirstUpperCase(adjAPlural) + getRandomSubject() + verbA + ". " + "\n" + toFirstUpperCase(nounASingular) + " " + adjASingular + ".";
+        String[] variants = new String[]{variantThree, variantFour};
+
+        return variants[random.nextInt(2)];
     }
 }
--- app/src/main/java/com/skynet/phrasegenerator/generators/Generator.java
+++ app/src/main/java/com/skynet/phrasegenerator/generators/Generator.java
@@ -1,10 +1,37 @@
 package com.skynet.phrasegenerator.generators;
 
+import java.util.Random;
+
 public abstract class Generator {
 
     int randomPhraseNumber = 0;
 
     public abstract String generatePhrase();
+
+    public String getRandomUnion() {
+        Random random = new Random();
+        String unionOne = " - это ";
+        String unionTwo = " - это не ";
+        String unionThree = " есть ";
+        String[] unions = new String[]{unionOne, unionTwo, unionThree};
+        return unions[random.nextInt(unions.length)];
+    }
+
+    public String getRandomProperty() {
+        Random random = new Random();
+        String propOne = " обладают свойством ";
+        String propTwo = " не обладают свойством ";
+        String[] properties = new String[]{propOne, propTwo};
+        return properties[random.nextInt(properties.length)];
+    }
+
+    public String getRandomSubject() {
+        Random random = new Random();
+        String subjOne = " предметы ";
+        String subjTwo = " объекты ";
+        String[] subjects = new String[]{subjOne, subjTwo};
+        return subjects[random.nextInt(subjects.length)];
+    }
 
     public String toFirstUpperCase(String word) {
         if (word == null || word.equals("")) return "";
